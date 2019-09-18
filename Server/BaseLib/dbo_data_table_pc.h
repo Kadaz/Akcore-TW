fa4485365dad92899d4e89aa12104705ca921102
TolisM <Kadaz@users.noreply.github.com>
2019-09-18 15:32:35 +0300

    Remapped PC Table

--- Server/BaseLib/dbo_data_table_pc.h
+++ Server/BaseLib/dbo_data_table_pc.h
@@ -8,36 +8,32 @@
 
 typedef struct
 {
-#if DBO_VERSION_NEW
+
 	uint32_t basic_lp;
-#else
-	uint16_t basic_lp;
-#endif
+
+
 
 	uint16_t basic_ep;
 
 	uint16_t basic_physical_defence;
 	uint16_t basic_energy_defence;
 
-#if DBO_VERSION_NEW
+
 	uint16_t str;
 	uint16_t con;
 	uint16_t foc;
 	uint16_t dex;
 	uint16_t sol;
 	uint16_t eng;
-#else
-	uint8_t str;
-	uint8_t con;
-	uint8_t foc;
-	uint8_t dex;
-	uint8_t sol;
-	uint8_t eng;
-#endif
+
+	//uint16_t wUnknownWRD;
 
 	float scale;
 
 	uint16_t attack_speed_rate;
+
+	//uint16_t wUnknownWRD2;
+
 	uint8_t attack_type;
 
 	float attack_range;
@@ -47,6 +43,9 @@ typedef struct
 	uint16_t block_rate;
 	uint16_t curse_success_rate;
 	uint16_t curse_tolerance_rate;
+
+	uint8_t byUnknownBYTE;
+	//uint16_t wUnknownWRD3;
 
 	float radius;
 
@@ -61,9 +60,13 @@ typedef struct
 
 	dbo_data_table_char_st table_char;
 
+	//uint16_t wUnknownWRD4;
+
 	uint8_t race;
 	uint8_t gender;
 	uint8_t clazz;
+
+	//uint8_t byUnknownBYTE2;
 
 	DWORD previous_class_id;
 
@@ -72,22 +75,22 @@ typedef struct
 	char model_child[NTL_MAX_LENGTH_MODEL_NAME + 1];
 	char model_adult[NTL_MAX_LENGTH_MODEL_NAME + 1];
 
+	//uint16_t wUnknownWRD5;
+
 	float child_run_speed_origin;
 	float child_run_speed;
 	float adult_run_speed_origin;
 	float adult_run_speed;
 
-#if DBO_VERSION_NEW
+
 	float unknown1[12];
-#endif
-	
-	uint16_t basic_lp;
+
+	uint16_t Basic_RP;
 	uint16_t basic_physical_offence;
 	uint16_t basic_energy_offence;
 
-#if DBO_VERSION_NEW
-	uint32_t unknown2;
-#endif
+	//uint16_t wUnknownWRD6;
+	uint16_t wUnknownWRD7;
 
 	uint8_t level_up_lp;
 	uint8_t level_up_ep;
@@ -97,6 +100,9 @@ typedef struct
 	uint8_t level_up_energy_offence;
 	uint8_t level_up_energy_defence;
 	
+	uint16_t wUnknownWRD8;
+	//uint8_t wUnknownBYTE3;
+
 	float level_up_str;
 	float level_up_con;
 	float level_up_foc;
